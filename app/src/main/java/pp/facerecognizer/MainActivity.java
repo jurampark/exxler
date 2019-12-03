2b1a8d38a75f933d329ccc305763f012dd42b138
Juram Park <jurampark@google.com>
2019-12-03 14:12:13 +0900

    improve main activity with list view

--- app/src/main/java/pp/facerecognizer/MainActivity.java
+++ app/src/main/java/pp/facerecognizer/MainActivity.java
@@ -30,17 +30,25 @@ import android.media.ImageReader.OnImageAvailableListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Size;
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import com.google.android.material.textfield.TextInputEditText;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -61,12 +69,53 @@ import pp.facerecognizer.tracking.MultiBoxTracker;
 public class MainActivity extends AppCompatActivity {
 
   private FloatingActionButton button;
+  private List<String> users = new ArrayList<>();
+  private ListView userListView;
+  private TextInputEditText usernameInput;
+  private ArrayAdapter<String> userListViewAdapter;
 
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.actiivty_main);
+
+    userListView = findViewById(R.id.userListView);
+    usernameInput = findViewById(R.id.usernameInput);
+
+    try {
+      BufferedReader reader = new BufferedReader(
+          new InputStreamReader(getAssets().open("users")));
+
+      // do reading, usually loop until end of file reading
+      String mLine;
+      while ((mLine = reader.readLine()) != null) {
+        users.add(mLine);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    userListViewAdapter = new ArrayAdapter<String>(this,
+        R.layout.user_list_item, R.id.username, users);
+    userListView.setAdapter(userListViewAdapter);
+
+    usernameInput.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        userListViewAdapter.getFilter().filter(charSequence);
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+
+      }
+    });
 
     button = findViewById(R.id.camera_button);
     button.setOnClickListener(view ->
